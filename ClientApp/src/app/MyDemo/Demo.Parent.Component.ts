a4c283e17070283bf74d544a87612d21f028b708
KANJI_HUANG <Kanji_Huang@ms.dlp.com.tw>
2020-03-30 17:21:04 +0800

    test

--- ClientApp/src/app/MyDemo/Demo.Parent.Component.ts
+++ ClientApp/src/app/MyDemo/Demo.Parent.Component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 /**
- * 父組件 
+ * 父組件  test
  * 父组件在其模板中通过选择器demo-child引用子组件DemoChildComponent，
  * 并通过子组件的两个输入属性paramOne和paramTwo向子组件传递数据，
  * 最后在子组件的模板中就显示传递给paramOne的数据和传递给paramTwo的数据这两行文本。
