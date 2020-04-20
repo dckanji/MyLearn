10fceeccca94611ae9d2a12ddf3e10c8b8dcb07d
KANJI_HUANG <Kanji_Huang@ms.dlp.com.tw>
2020-04-20 11:58:21 +0800

    註解說明0417

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using System.Data;
 using learn.Services;
 using Microsoft.AspNetCore.Mvc;
+using learn.Models;
+using System;
 
 namespace learn.Controllers
 {
@@ -17,17 +19,21 @@ namespace learn.Controllers
         [HttpGet("get-user")]
         public ActionResult GetUser()
         {
+        /* 第一種 資料庫模式
             DataTable users = this.service.GetUser();
             return Ok(users);
-            
+        */
 
-            // var users = new User[]
-            // {
-            //     new User { Id = 1, Name= "xxx", Age = 11, CreationDate = DateTime.Now },
-            //     new User { Id = 2, Name= "ooo", Age = 18, CreationDate = DateTime.Now },
-            // };
-            // return Ok(users);
-            //throw new System.NotImplementedException();
+        //直接建立類別模式...需要引入 using learn.Models 和 using System;
+             var users = new User[]
+             {
+                 new User { Id = 1, Name= "xxx", Age = 11, CreationDate = DateTime.Now },
+                 new User { Id = 2, Name= "ooo", Age = 18, CreationDate = DateTime.Now },
+             };
+             return Ok(users);
+
+            throw new System.NotImplementedException();
+
         }
     }
 }
